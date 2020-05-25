29adf1a1dc96cab9c39c96d018ea622fe17e2da9
Renato <rmontufar1792@gmail.com>
2020-05-25 00:15:52 -0600

    Variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://deus_magna:zXwYuGd8qIAhExpz@cluster0-ny9yu.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;

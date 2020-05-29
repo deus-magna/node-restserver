c02f44241ff776e5b126c82099fb5d61db3cd7ac
Renato <rmontufar1792@gmail.com>
2020-05-28 19:21:45 -0600

    Subir archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -22,6 +22,27 @@ let verificaToken = (req, res, next) => {
     });
 };
 
+// Verificar Token
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+};
+
 // Verifica AdminRole
 let verificaAdminRole = (req, res, next) => {
 
@@ -44,5 +65,6 @@ let verificaAdminRole = (req, res, next) => {
 
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 }
